1e07270f23cf4668e568e62d3228e0f8f75abd1f
frank19994 <franciscoo638@gmail.com>
2025-03-29 21:22:29 -0600

    url actualizada

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-0627d27b-a6c6-4085-9220-ad384f28a185.containerhub.tripleten-services.com/?lng=es'
+urban_routes_url = 'https://cnt-01213053-d024-4dff-98fe-79af1752bfc9.containerhub.tripleten-services.com/?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
